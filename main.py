092c99ff9a31b02edca2c979fc8a0ac8617c6bfd
ZahrizhalAli <zzahrizhal.ali@gmail.com>
2022-03-15 23:20:57 +0800

    Trying to fetch the desc

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ em_web = response.text
 soup = BeautifulSoup(em_web, "html.parser")
 
 all_movies = soup.find_all(name="h2", class_="ct-slideshow__slide__text-container__caption")
+all_desc = soup.select(selector="div.paragraph--view-mode--full:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > p:nth-child(1)")
 
 list_movies = [movie.getText() for movie in all_movies]
-list_movies.pop(0);
-movies = list_movies[::-1]
+list_movies.pop(0)
+# list_desc = [desc.getText() for desc in all_desc]
 
-with open("movies.txt", mode="w", encoding="utf8") as file:
-    for movie in movies:
-        file.write(movie)
+print(all_desc)
+movies = list_movies[::-1]
+# descs = list_desc[::-1]
+# with open("movies.txt", mode="w", encoding="utf8") as file:
+#
+#     for movie in movies:
+#         file.write(movie)
+#
 
